defc3478962bee24dac82d5233887cc934273ff5
tarifalhasan <tarifalhasan999@gmail.com>
2024-04-01 21:54:54 +0600

    up to project

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,112 +1,98 @@
 import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
-
-import Counter from "../components/starter/counter/counter";
-import Hero from "../components/starter/hero/hero";
-import Infobox from "../components/starter/infobox/infobox";
-import Starter from "../components/starter/next-steps/next-steps";
-
+import { FaTelegram, FaXTwitter } from "@qwikest/icons/font-awesome";
+import line from "/Line1.png";
+import catImage from "/cat.png";
+import dex_scanner from "/dex-scanner.svg";
+import dex_tool from "/dex-tool.svg";
+import logo from "/logo.svg";
 export default component$(() => {
   return (
-    <>
-      <Hero />
-      <Starter />
-
-      <div role="presentation" class="ellipsis"></div>
-      <div role="presentation" class="ellipsis ellipsis-purple"></div>
-
-      <div class="container container-center container-spacing-xl">
-        <h3>
-          You can <span class="highlight">count</span>
-          <br /> on me
-        </h3>
-        <Counter />
+    <div class="min-h-screen  relative overflow-hidden bg-black text-white">
+      <div class="flex w-full  px-4 lg:px-10 py-6">
+        <img width={112} height={34} src={logo} alt="logo" />
       </div>
-
-      <div class="container container-flex">
-        <Infobox>
-          <div q:slot="title" class="icon icon-cli">
-            CLI Commands
+      <div
+        class="w-full px-4 container mx-auto flex lg:flex-row
+      flex-col absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 lg:items-center gap-6"
+      >
+        <div class="basis-full space-y-6 lg:basis-1/2">
+          <h2 class=" text-[#FFE167] leading-none font-normal text-[2.5rem] md:text-[4.25rem] lg:text-[3rem] xl:text-[4rem] 2xl:text-[4.25rem]">
+            Miguel / Michael,{" "}
+          </h2>
+          <p class="text-xl md:text-4xl lg:text-2xl xl:text-4xl font-normal text-[#9B9B9B]">
+            also known as <span class="text-white">Standing Kitten</span>,{" "}
+            <br class="hidden md:block" /> is a kitten that appeared to be
+            standing in a video.{" "}
+          </p>
+          <div class="sm:max-w-[269px] flex basis-1/2 w-1/2 sm:w-full sm:basis-full items-center sm:gap-5 sm:ml-20 2xl:ml-[10rem]">
+            <img
+              src={line}
+              alt="lied"
+              width={1}
+              class="hidden sm:block "
+              height={47}
+            />
+            <p class="  text-base font-normal sm:text-2xl lg:text-base xl:text-xl 2xl:text-2xl  text-[#A5A5A5]">
+              The video of the kitten went viral in 2020.
+            </p>
           </div>
-          <>
-            <p>
-              <code>npm run dev</code>
-              <br />
-              Starts the development server and watches for changes
-            </p>
-            <p>
-              <code>npm run preview</code>
-              <br />
-              Creates production build and starts a server to preview it
-            </p>
-            <p>
-              <code>npm run build</code>
-              <br />
-              Creates production build
-            </p>
-            <p>
-              <code>npm run qwik add</code>
-              <br />
-              Runs the qwik CLI to add integrations
-            </p>
-          </>
-        </Infobox>
-
-        <div>
-          <Infobox>
-            <div q:slot="title" class="icon icon-apps">
-              Example Apps
-            </div>
-            <p>
-              Have a look at the <a href="/demo/flower">Flower App</a> or the{" "}
-              <a href="/demo/todolist">Todo App</a>.
-            </p>
-          </Infobox>
-
-          <Infobox>
-            <div q:slot="title" class="icon icon-community">
-              Community
-            </div>
-            <ul>
-              <li>
-                <span>Questions or just want to say hi? </span>
-                <a href="https://qwik.builder.io/chat" target="_blank">
-                  Chat on discord!
-                </a>
-              </li>
-              <li>
-                <span>Follow </span>
-                <a href="https://twitter.com/QwikDev" target="_blank">
-                  @QwikDev
-                </a>
-                <span> on Twitter</span>
-              </li>
-              <li>
-                <span>Open issues and contribute on </span>
-                <a href="https://github.com/BuilderIO/qwik" target="_blank">
-                  GitHub
-                </a>
-              </li>
-              <li>
-                <span>Watch </span>
-                <a href="https://qwik.builder.io/media/" target="_blank">
-                  Presentations, Podcasts, Videos, etc.
-                </a>
-              </li>
-            </ul>
-          </Infobox>
+        </div>
+        <div class="basis-full sm:gap-4 gap-10  lg:basis-1/2 flex flex-col sm:flex-row  lg:flex-row sm:items-center sm:justify-center ">
+          <div class=" order-last sm:order-first  flex-col xl:absolute mt-6  lg:left-[2%] lg:-bottom-14 xl:-bottom-2 sm:max-w-max mx-auto   sm:gap-5  grid grid-cols-2 sm:flex gap-8 sm:flex-col ">
+            <a
+              href="/"
+              target="_blank"
+              class="text-base gap-3 flex  items-center font-normal text-white transition-all  duration-700 hover:text-[#FFE167]"
+            >
+              <FaTelegram class=" w-4 h-4" />
+              <span>Telegram</span>
+            </a>
+            <a
+              href="/"
+              target="_blank"
+              class="text-base gap-3 flex  items-center font-normal text-white transition-all duration-700 hover:text-[#FFE167]"
+            >
+              <FaXTwitter class=" w-4 h-4" />
+              <span>X</span>
+            </a>
+            <a
+              href="/"
+              target="_blank"
+              class="text-base gap-3 flex basis-1/2 w-1/2 sm:w-full sm:basis-full items-center font-normal text-white transition-all hover:fill-[#ffe167] duration-700 hover:text-[#FFE167]"
+            >
+              <img
+                src={dex_scanner}
+                alt="lied"
+                width={20}
+                class=" w-5 h-5 block "
+                height={20}
+              />
+              <span>Dexscreener</span>
+            </a>
+            <a
+              href="/"
+              target="_blank"
+              class="text-base gap-3 flex basis-1/2 w-1/2 sm:w-full sm:basis-full items-center font-normal text-white transition-all hover:fill-[#ffe167] duration-700 hover:text-[#FFE167]"
+            >
+              <img
+                src={dex_tool}
+                alt="lied"
+                width={20}
+                class="block "
+                height={20}
+              />
+              <span>Dextools</span>
+            </a>
+          </div>
+          <img
+            width={651}
+            class="max-w-[214px] block mx-auto sm:max-w-[320px] lg:max-w-[345px] xl:max-w-[480px] 2xl:max-w-[651px]"
+            height={739}
+            src={catImage}
+            alt="logo"
+          />
         </div>
       </div>
-    </>
+    </div>
   );
 });
-
-export const head: DocumentHead = {
-  title: "Welcome to Qwik",
-  meta: [
-    {
-      name: "description",
-      content: "Qwik site description",
-    },
-  ],
-};
--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,9 +1,6 @@
 import { component$, Slot, useStyles$ } from "@builder.io/qwik";
-import { routeLoader$ } from "@builder.io/qwik-city";
 import type { RequestHandler } from "@builder.io/qwik-city";
-
-import Header from "../components/starter/header/header";
-import Footer from "../components/starter/footer/footer";
+import { routeLoader$ } from "@builder.io/qwik-city";
 
 import styles from "./styles.css?inline";
 
@@ -28,11 +25,9 @@ export default component$(() => {
   useStyles$(styles);
   return (
     <>
-      <Header />
       <main>
         <Slot />
       </main>
-      <Footer />
     </>
   );
 });
